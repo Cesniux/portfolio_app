0d2e9303cdd163361b85d5fe5e8c7470f68ae9b9
Cesniux <cesniux@gmail.com>
2022-12-15 22:03:51 +0200

    home page - app bar, profile picture

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio_app/style/res/constants.dart';
+import 'package:portfolio_app/widgets/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-      
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: CColor.backgroundColorBright,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomeView(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-  
     return Scaffold(
-      appBar: AppBar(
-   
-        title: Text(widget.title),
-      ),
-      body: Center(
-  
+      appBar: const CustomAppBar(),
+      body: SingleChildScrollView(
         child: Column(
- 
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          children: [
+            Center(
+              child: Container(
+                decoration: BoxDecoration(shape: BoxShape.circle, boxShadow: [
+                  BoxShadow(
+                    offset: const Offset(-5.0, 8),
+                    color: CColor.backgroundColorDifferent,
+                    spreadRadius: 3,
+                  )
+                ]),
+                child: ClipOval(
+                    child: Image.asset(
+                  'assets/images/profile_image.jpeg',
+                  width: 180,
+                  height: 180,
+                  fit: BoxFit.cover,
+                )),
+              ),
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
---
+++ lib/style/res/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CColor {
+  static Color backgroundColorBright = const Color(0xFFE5E5E5);
+  static Color backgroundColorDifferent = const Color(0xFFEDF7FA);
+  static Color textColor = const Color(0xFF21243D);
+  static Color buttonColor = const Color(0xFFFF6464);
+  static Color labelColor = const Color(0xFF8695A4);
+}
